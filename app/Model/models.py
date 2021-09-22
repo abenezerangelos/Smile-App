344312c9a9387d232a5e80f01e07a10510e6617b
AnthBoss <anthony.salvione@wsu.edu>
2021-09-21 17:27:45 -0700

    Added a model called postTags that contains the post_id and tag_id. Also added a __repr__ method that prints these tags

--- app/Model/models.py
+++ app/Model/models.py
@@ -1,6 +1,14 @@
 from datetime import datetime
 from app import db
 
+class PostTags(db.Model):
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'), primary_key=True)
+    def __repr__(self):
+        return ('Id of the tag: {} Name of the tag: {}'.format(self.post_id, self.tag_id))
+
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150))
@@ -8,3 +16,10 @@ class Post(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     likes = db.Column(db.Integer, default = 0)
     happiness_level = db.Column(db.Integer, default = 3)
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.String(20))
+
+
+
