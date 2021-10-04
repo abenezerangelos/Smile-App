742b7750c6043cb43d4d9e8b12a0b110feed2580
AnthBoss <anthony.salvione@wsu.edu>
2021-10-03 17:39:51 -0700

    Task 2 part 2 - added __repr__, set_password, get_password functions to the User model

--- app/Model/models.py
+++ app/Model/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
 postTags = db.Table('post_tags',
@@ -32,5 +33,15 @@ class User(db.Model):
     username = db.Column(db.String(64), unique = True)
     email = db.Column(db.String(120), unique = True)
     password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return ('id: {} username: {}'.format(self.id, self.username))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def get_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
 
 
